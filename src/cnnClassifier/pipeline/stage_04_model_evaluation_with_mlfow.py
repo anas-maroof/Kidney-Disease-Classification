088e33aafc81a00956d9324ab8953675408d4611
Anas Maroof <anas-maroof@users.noreply.github.com>
2024-04-19 16:52:43 +0530

    Model Evaluation using MLFlow and DagsHub

--- src/cnnClassifier/pipeline/stage_04_model_evaluation_with_mlfow.py
+++ src/cnnClassifier/pipeline/stage_04_model_evaluation_with_mlfow.py
@@ -1,26 +1,35 @@
 from cnnClassifier.config.configuration import ConfigurationManager
-from cnnClassifier.components.prepare_base_model import Training
+from cnnClassifier.components.model_evaluation_with_mlflow import Evaluation
 from cnnClassifier import logger
 
-STAGE_NAME = "Model Training"
 
-class ModelTrainingPipeline:
+
+STAGE_NAME = "Evaluation stage"
+
+
+class EvaluationPipeline:
     def __init__(self):
         pass
+
     def main(self):
         config = ConfigurationManager()
-        training_config = config.get_training_config()
-        training = Training(config=training_config)
-        training.get_base_model()
-        training.train_valid_generator()
-        training.train()
+        eval_config = config.get_evaluation_config()
+        evaluation = Evaluation(eval_config)
+        evaluation.evaluation()
+        evaluation.save_score()
+        # evaluation.log_into_mlflow()
+
+
+
 
 if __name__ == '__main__':
     try:
+        logger.info(f"*******************")
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = ModelTrainingPipeline()
+        obj = EvaluationPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
         logger.exception(e)
         raise e
+            

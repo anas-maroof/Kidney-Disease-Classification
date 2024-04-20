5d2bcd6c2899fa6397c6cf46bf115d4404bbcb74
Anas Maroof <anas-maroof@users.noreply.github.com>
2024-04-20 19:44:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -44,5 +44,5 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 
 except Exception as e:
-        logger.exception(e)
-        raise e
+    logger.exception(e)
+    raise e

5808182ffa5d862f74668ae7689d213deec05051
Anas Maroof <anas-maroof@users.noreply.github.com>
2024-05-01 23:17:53 +0530

    User Interface Done

--- src/cnnClassifier/pipeline/stage_03_model_training.py
+++ src/cnnClassifier/pipeline/stage_03_model_training.py
@@ -1,5 +1,5 @@
 from cnnClassifier.config.configuration import ConfigurationManager
-from cnnClassifier.components.prepare_base_model import Training
+from cnnClassifier.components.model_training import Training
 from cnnClassifier import logger
 
 STAGE_NAME = "Model Training"
